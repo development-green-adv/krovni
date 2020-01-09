f28fbc55b224725f7f9a53913c50acf532d967c2
Nemanja-Danev <nemanja.danev@green-adv.rs>
2020-01-09 16:20:07 +0100

    fixed left meni

--- resources/views/inc/left-meni.blade.php
+++ resources/views/inc/left-meni.blade.php
@@ -17,62 +17,18 @@
                 <a href="#">Krovni koferi cam</a>
             </li>
         </ul>
-        <li data-toggle="collapse" href="#multiCollapseExample2" role="button" aria-expanded="false" aria-controls="multiCollapseExample2" class="collapsed li-drop">
+        <li class="li-drop">
             KROVNI NOSAČI
         </li>
-        <ul class="multi-collapse collapse left-meni-items-inner" id="multiCollapseExample2"> 
-            <li>
-                <a href="#">Krovni koferi thule</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi g3</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi cam</a>
-            </li>
-        </ul>
-        <li data-toggle="collapse" href="#multiCollapseExample3" role="button" aria-expanded="false" aria-controls="multiCollapseExample3" class="collapsed li-drop">
+        <li class="li-drop">
             RENTIRAJTE ONLINE
         </li>
-        <ul class="multi-collapse collapse left-meni-items-inner" id="multiCollapseExample3"> 
-            <li>
-                <a href="#">Krovni koferi thule</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi g3</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi cam</a>
-            </li>
-        </ul>
-        <li data-toggle="collapse" href="#multiCollapseExample4" role="button" aria-expanded="false" aria-controls="multiCollapseExample4" class="collapsed li-drop">
+        <li class="li-drop">
             GALERIJA
         </li>
-        <ul class="multi-collapse collapse left-meni-items-inner" id="multiCollapseExample4"> 
-            <li>
-                <a href="#">Krovni koferi thule</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi g3</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi cam</a>
-            </li>
-        </ul>
-        <li data-toggle="collapse" href="#multiCollapseExample5" role="button" aria-expanded="false" aria-controls="multiCollapseExample5" class="collapsed li-drop">
+        <li class="li-drop">
             KONTAKT
         </li>
-        <ul class="multi-collapse collapse left-meni-items-inner" id="multiCollapseExample5"> 
-            <li>
-                <a href="#">Krovni koferi thule</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi g3</a>
-            </li>
-            <li>
-                <a href="#">Krovni koferi cam</a>
-            </li>
-        </ul>
     </ul>
 </ul>
 </div>
